2fc7b0bfd66759ee30306a5f00e05039bf4d462f
Aman Srivastava <amansrivastava905@gmail.com>
2021-01-04 12:43:06 +0530

    Signed-off-by: Aman Srivastava <amansrivastava905@gmail.com>

--- src/pages/ContentPage.js
+++ src/pages/ContentPage.js
@@ -2,20 +2,12 @@ import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import LevelOne from '../images/level-1-back.png';
 import { module1 } from '../data/data.js'
+import CardContent from '../Components/CardContent';
+import CardQuestionComponent from '../Components/CardQuestionComponent';
 
-// console.log(module1);
-
-// console.log(module1.Items);
 
 const ContentCard = () => {
 
-    // random number generator
-    const random = Math.round(Math.random());
-
-    // check option correct or not
-    const checkOption = (e)=>{
-       alert(e.target.outerText);
-    }
 
     return (
         <MainContainer>
@@ -28,37 +20,12 @@ const ContentCard = () => {
                         module1.Slides.map((slide) => {
                             if (slide.type === 'S') {
                                 return (
-                                    <Div key={slide.id}>
-                                        <Heading>{slide.heading}</Heading>
-                                        <Subject>
-                                            {slide.material.map((e) => {
-                                                return (
-                                                    <Block>
-                                                        {e.hindi}<Span> = </Span>{e.english}<Span> = </Span>{e.sanskrit}<Span> = </Span>{e.translation}
-                                                    </Block>
-                                                )
-                                            })}
-                                        </Subject>
-                                    </Div>
+                                    <CardContent slideData={slide} />
                                 )
                             }
                             else {
                                 return (
-                                    <Div key={slide.id}>
-                                        <Heading>{slide.heading}</Heading>
-                                        <Subject>
-                                            <Question>
-                                                {slide.material[random].question}
-                                            </Question>
-                                            <OptionContainer>
-                                                {slide.material[random].options.map((e) => {
-                                                    return (
-                                                        <Option onClick={checkOption} id={slide.id}>{e}</Option>
-                                                    )
-                                                })}
-                                            </OptionContainer>
-                                        </Subject>
-                                    </Div>
+                                    <CardQuestionComponent slideData={slide} />
                                 )
                             }
                         })
@@ -72,87 +39,7 @@ const ContentCard = () => {
 export default ContentCard;
 
 
-const OptionContainer = styled.div`
-    margin-top:30px;
-    display:flex;
-    justify-content:center;
-    flex-wrap:wrap;
-    width:60vw;
-    @media screen and (max-width:900px)
-    {
-        width:70vw;
-    }
-    @media screen and (max-width:500px)
-    {
-        width:90vw;
-    }
-`
 
-const Option = styled.button`
-    background-color:rgba(63,81,181,0.2);
-    border:1px solid rgba(63,81,181,0.5);
-    cursor:pointer;
-    padding:10px 20px;
-    margin:10px;
-    outline:none;
-    transition:all 0.4s;
-    border-radius:30px;
-    font-size:24px;
-    color:darkblue;
-    &:hover{
-        background-color:rgba(63,81,181,0.3);
-        border:1px solid rgba(63,81,181,0.6);
-        transition:all 0.4s;
-    }
-`
-
-const Question = styled.div`
-    padding:20px;
-    color:black;
-    font-weight:bold;
-    font-size:24px;
-    background-color:rgba(63,81,181,0.1);
-    width:60vw;
-    text-align:center;
-    @media screen and (max-width:900px)
-    {
-        width:70vw;
-    }
-    @media screen and (max-width:500px)
-    {
-        width:90vw;
-    }
-`
-
-const Subject = styled.div`
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-`
-const Block = styled.div`
-    padding:10px;
-    text-align:center;
-    background-color:rgba(63,81,181,0.1);
-    width:60vw;
-    border-bottom:0.5px dotted rgba(63,81,181,0.4);
-    font-size:18px;
-    color:rgba(0,0,0,0.7);
-    @media screen and (max-width:900px)
-    {
-        width:70vw;
-    }
-    @media screen and (max-width:500px)
-    {
-        width:90vw;
-    }
-    display:flex;
-    justify-content:space-around;
-    flex-wrap:wrap;
-`
-const Span = styled.span`
-    color:darkblue;
-`
 const MainContainer = styled.div`
     background-image:url(${LevelOne});
     background-size:800px;
@@ -160,30 +47,7 @@ const MainContainer = styled.div`
     background-attachment:fixed;
     background-position:center bottom;
 `
-const Div = styled.div`
-    width:60vw;
-    min-height:70vh;
-    background-color:rgba(63,81,181,0.05);
-    border:2px solid rgba(63,81,181,0.1);
-    border-radius:20px;
-    padding:20px;
-    margin:20px 10px;
-    backdrop-filter: blur(15px) saturate(100%) contrast(55%) brightness(120%);
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
 
-    @media screen and (max-width:900px)
-    {
-        width:70vw;
-    }
-    @media screen and (max-width:500px)
-    {
-        width:90vw;
-    }
-
-`
 const Header = styled.div`
     background-color:rgba(63,81,181,0.05);
     backdrop-filter: blur(5px) saturate(100%) contrast(55%) brightness(120%);
@@ -202,7 +66,6 @@ const Head = styled.h1`
 
 const ContentContainer = styled.div`
     padding:80px 10px;
-    background-color:rgba(0,0,0,0.1);
     min-height:100vh;
     display:flex;
     justify-content:center;
@@ -210,11 +73,4 @@ const ContentContainer = styled.div`
 
 const CardsContainer = styled.div`
  
-`
-
-const Heading = styled.h4`
-    color:darkblue;
-    font-size:36px;
-    padding:20px 0px;
-    margin-bottom:20px;
 `
